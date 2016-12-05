e32d9a77fce2e6e0154a87854a561ac740ae1ab2
kig <kig@kig.local>
2016-12-05 21:00:15 +0200

    simple max function

---
+++ python/max_of_list.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+
+"""The code below take multiple arguments from command line and return the bigest one"""
+import sys
+
+def main():
+    max_number = 0
+    for i in range(len(sys.argv[1:])+1):
+        for j in range(i+1, len(sys.argv[1:])+1):
+            
+            if max_number > sys.argv[i] and max_number > sys.argv[j]:
+                max_number = max_number
+            elif max_number < sys.argv[i] and sys.argv[i] > sys.argv[j]:
+                max_numer = sys.argv[i]
+            elif sys.argv[j] > sys.argv[i] and max_number < sys.argv[j]:
+                max_number = sys.argv[j]
+    print max_number
+
+if __name__=="__main__":
+    main() 
