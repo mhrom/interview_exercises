465bf003d6ffe37672ab74d2d990e4df51dd199c
Mykola Hromivchuk <mhromivchuk@gmail.com>
2019-08-09 19:46:34 +0300

    print lyrics of some song

---
+++ python/print_lyrics_of_song.py
@@ -0,0 +1,5 @@
+#!/bin/python
+
+song="It's my life\nIt's now or never\nI ain't gonna live forever\nI just want to live while I'm alive\n"
+
+print song
---
+++ python/print_name.py
@@ -0,0 +1,4 @@
+#!/bin/python
+
+name = raw_input("Please type your name:")
+print 'Hello ' + name + '. How are you doing?'   
