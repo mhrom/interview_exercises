ae036753b30cd68540c8ebe4619e6bdb2c80c977
Mykola Hromivchuk <mhromivchuk@gmail.com>
2019-08-10 11:41:14 +0300

    add summation of several numbers

---
+++ python/summation.py
@@ -0,0 +1,7 @@
+#!/bin/python
+
+number1 = 64
+number2 = 32
+
+total = number1 + number2
+print total
---
+++ python/summation2.py
@@ -0,0 +1,7 @@
+#!/bin/python
+
+x = raw_input("Please enter number x: ")
+y = raw_input("Please enter number y: ")
+
+total = int(x) + int(y)
+print total
